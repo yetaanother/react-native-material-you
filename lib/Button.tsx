c592ec989b9f1c78460f6e4acfaf0fd3d63c3d4e
Prashant <prashantchaubey9795@gmail.com>
2021-11-11 09:23:14 +0000

    Creating inner layer instead of manipulating opacity

--- lib/Button.tsx
+++ lib/Button.tsx
@@ -37,42 +37,50 @@ export const Button: FunctionComponent<ButtonProps> = ({
 
   const render = () => {
     return (
-      <View style={getOpacityLayerStyles()}>
-        <NativePressable style={getButtonStyles()} onPress={onPress}>
-          {renderContent()}
-        </NativePressable>
-      </View>
+      <NativePressable style={getButtonStyles()} onPress={onPress}>
+        <View style={getInnerStyles()}>{renderContent()}</View>
+      </NativePressable>
     );
   };
 
   const getButtonStyles = () => {
-    let buttonStyles: ViewStyle | TextStyle | ImageStyle = { ...styles.parent };
-    // States
-    if (state === "disabled") {
-      buttonStyles = { ...buttonStyles, ...styles.buttonDisabled };
-    } else if (state === "hovered") {
-      buttonStyles = { ...buttonStyles, ...styles.boxShadow };
-    }
-    //Icon
-    if (icon) {
-      buttonStyles = { ...buttonStyles, ...styles.buttonWithIcon };
+    let buttonStyles: ViewStyle | TextStyle | ImageStyle = { ...styles.button };
+
+    if (type === "filled") {
+      if (state === "disabled") {
+        buttonStyles = { ...buttonStyles, ...styles.buttonDisabled };
+      } else if (state === "hovered") {
+        buttonStyles = { ...buttonStyles, ...styles.boxShadow };
+      }
+    } else if (type === "outlined") {
+      buttonStyles = { ...buttonStyles, ...styles.buttonOutlined };
     }
     return { ...buttonStyles, ...style };
   };
 
-  const getOpacityLayerStyles = () => {
-    if (state === "hovered") {
-      return { ...styles.opacity92 };
-    } else if (state === "focused" || state === "pressed") {
-      return { ...styles.opacity96 };
-    } else {
-      return {};
+  const getInnerStyles = () => {
+    let innerStyles = { ...styles.inner };
+    if (type == "filled") {
+      if (state === "hovered") {
+        innerStyles = { ...innerStyles, ...styles.innerHovered };
+      } else if (state === "focused" || state === "pressed") {
+        innerStyles = { ...innerStyles, ...styles.innerFocusedOrPressed };
+      }
     }
+    if (type === "outlined") {
+      if (state === "hovered") {
+        innerStyles = { ...innerStyles, ...styles.innerOutlinedHovered };
+      }
+    }
+
+    if (icon) {
+      innerStyles = { ...innerStyles, ...styles.innerWithIcon };
+    }
+    return innerStyles;
   };
 
   const renderContent = () => {
     let textStyles = getTextStyles();
-
     //https://icons.expo.fyi/
     if (icon) {
       return (
@@ -92,8 +100,12 @@ export const Button: FunctionComponent<ButtonProps> = ({
 
   const getTextStyles = () => {
     let textStyles = { ...styles.text };
-    if (state === "disabled") {
-      textStyles = { ...textStyles, ...styles.textDisabled };
+    if (type === "filled") {
+      if (state === "disabled") {
+        textStyles = { ...textStyles, ...styles.textDisabled };
+      }
+    } else if (type === "outlined") {
+      textStyles = { ...textStyles, ...styles.textOutlined };
     }
     return textStyles;
   };
@@ -110,20 +122,37 @@ export const Button: FunctionComponent<ButtonProps> = ({
 };
 
 const styles = StyleSheet.create({
-  parent: {
-    alignItems: "center",
-    justifyContent: "center",
-    display: "flex",
-    flexDirection: "row",
-    paddingVertical: 10,
-    paddingHorizontal: 24,
+  button: {
     borderRadius: 100,
     backgroundColor: "#6750A4",
   },
   buttonDisabled: {
     backgroundColor: "rgba(31,31,31, 0.12)",
   },
-  buttonWithIcon: {
+  buttonOutlined: {
+    backgroundColor: "#FFFFFF",
+    borderStyle: "solid",
+    borderColor: "#79747E",
+    borderWidth: 1,
+  },
+  inner: {
+    alignItems: "center",
+    justifyContent: "center",
+    display: "flex",
+    flexDirection: "row",
+    paddingVertical: 10,
+    paddingHorizontal: 24,
+  },
+  innerHovered: {
+    backgroundColor: "rgba(255,255,255,0.08)",
+  },
+  innerFocusedOrPressed: {
+    backgroundColor: "rgba(255,255,255,0.12)",
+  },
+  innerOutlinedHovered: {
+    backgroundColor: "rgba(103, 80, 164, 0.08)",
+  },
+  innerWithIcon: {
     paddingLeft: 16,
     paddingRight: 24,
   },
@@ -134,17 +163,14 @@ const styles = StyleSheet.create({
     lineHeight: 20,
     fontWeight: "500",
     letterSpacing: 0.1,
-    color: "white",
+    color: "#FFFFFF",
   },
   textDisabled: {
     color: "#1C1B1F",
     opacity: 0.38,
   },
-  opacity92: {
-    opacity: 0.92,
-  },
-  opacity96: {
-    opacity: 0.96,
+  textOutlined: {
+    color: "#6750A4",
   },
   // https://ethercreative.github.io/react-native-shadow-generator/
   boxShadow: {
